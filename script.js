6c956f292cc92a01f0ed4c9e82e4ba52600faf58
Thulio33 <thuliofelipe78@icloud.com>
2025-05-08 20:44:21 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+function eviarFormulario(event){
+    event.preventDeFault();
+    const none =document.getElementById('nomeContato').value;
+    const email=document.getElementById('emailContato').value; 
+    const telefone=document.getElementById('telefoneContato').value;
+
+    const mensagem = `
+    Dados enviados com Sucesso!<br>
+    Nome: ${nome}<br>
+    Email: ${email}</br>
+    telefone: ${telefone}
+    `;
+
+    document.getElementById('mensagem-Confirmacao').innerHTML = mensagem;
+}
+
+function mostrarFatoGato() {
+    fetch("https://catfact.ninja/fact")
+    then(resposta => resposta.json())
+    then(dados =>{
+        document.getElementById('fato-gato').innerText = dados.fact;
+     } )
+     .catch(erro => console.error('erro ao buscar fato do gato:',erro));
+}
+
+function mostrarImagemCachorro(){
+    fetch("https://dog.ceo/api/breeeds/image/random")
+    .then(resposta => resposta.json())
+    .then(dados => {
+        document.getElementById('imagem-cachorro').scr = dados.menssagem;
+    })
+    .catch(erro => console.error('erro ao buscar imagem do cachorro:', erro));
+}
+
+mostrarImagemCachorro()
